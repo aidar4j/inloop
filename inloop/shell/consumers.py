ea0cacc6434c15e2bd292f7dd4015290a2a97ead
PhilippMatthes <github@philippmatth.es>
2019-08-06 14:50:20 +0200

    Try subprocess version (works, but deadlocks)

--- inloop/shell/consumers.py
+++ inloop/shell/consumers.py
@@ -1,40 +1,32 @@
 import asyncio
+import json
+import subprocess
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ShellConsumer(AsyncWebsocketConsumer):
 
-    async def spawn_container(self):
-        #loop = asyncio.get_event_loop() or asyncio.new_event_loop()
-        #child_watcher = asyncio.get_child_watcher()
-        #child_watcher.attach_loop(loop)
-        #print(loop, child_watcher)
-
-        # TODO: Use docker run -it openjdk /bin/jshell
-        args = "echo \"Hello Shell Consumer\"".split()
-        process = await asyncio.create_subprocess_exec(
-            *args, stdout=asyncio.subprocess.PIPE, loop=loop
+    async def execute(self, cmd):
+        args = cmd.split()
+        popen = subprocess.Popen(
+            args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
         )
-        stdout, stderr = await self.process.communicate()
-        print(stdout, stderr)
+        for line in iter(popen.stdout.readline, ""):
+            await asyncio.sleep(1.0)
+            await self.send(text_data=json.dumps({
+                "type": "line",
+                "content": line
+            }))
 
     async def connect(self):
-        self.user = self.scope["user"]
-
-        await self.channel_layer.group_add(str(self.user.id), self.channel_name)
         await self.accept()
 
-        await self.spawn_container()
+        await self.execute("docker run -it openjdk /bin/jshell")
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(str(self.user.id), self.channel_name)
+        print("Killing shell", self.popen.kill())
 
-    async def events_line(self, event):
-        await self.send({
-            "type": "events.line",
-            "content": event["line"]
-        })
 
     async def receive(self, text_data=None, bytes_data=None):
         pass
